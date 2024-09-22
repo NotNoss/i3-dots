2ba3082c3e004f3f579bee3498e625f140b2f5fb
NotNoss <nosstw@gmail.com>
2024-09-22 09:01:06 -0500

    Added AUR packages to update check script

--- .config/polybar/scripts/arch-updates.sh
+++ .config/polybar/scripts/arch-updates.sh
@@ -2,12 +2,18 @@
 #source https://github.com/x70b1/polybar-scripts
 #source https://github.com/polybar/polybar-scripts
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
+if ! updates_arch=$(checkupdates 2>/dev/null | wc -l); then
+  updates_arch=0
 fi
 
+if ! updates_aur=$(yay -Qum 2>/dev/null | wc -l); then
+  updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
+
 if [ $updates_arch -gt 0 ]; then
-    echo $updates_arch
+  echo $updates
 else
-    echo "0"
+  echo "0"
 fi
